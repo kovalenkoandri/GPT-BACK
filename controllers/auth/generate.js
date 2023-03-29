c4fc2d3c0ae9b2910ce0015530ca6e0250a57463
kovalenkoandri <kovalenkoandrii@gmail.com>
2023-03-29 13:18:57 +0300

    max_tokens: 32

--- controllers/auth/generate.js
+++ controllers/auth/generate.js
@@ -45,6 +45,7 @@ const generate = async (req, res) => {
       // prompt: generatePrompt(animal),
       prompt: animal,
       temperature: 0.6,
+      max_tokens: 32,
     });
     res.status(200).json({ result: completion.data.choices[0].text.trim() });
   } catch (error) {
