e144ea7d6adc018ff4320abef11ab1fb4dbe679b
kovalenkoandri <kovalenkoandrii@gmail.com>
2023-04-03 14:00:04 +0300

    animal changed for requestData

--- controllers/auth/generate.js
+++ controllers/auth/generate.js
@@ -6,19 +6,6 @@ const configuration = new Configuration({
 });
 const openai = new OpenAIApi(configuration);
 
-// const generatePrompt = animal => {
-//   const capitalizedAnimal =
-//     animal[0].toUpperCase() + animal.slice(1).toLowerCase();
-//   return `Suggest three names for an animal that is a superhero.
-
-// Animal: Cat
-// Names: Captain Sharpclaw, Agent Fluffball, The Incredible Feline
-// Animal: Dog
-// Names: Ruff the Protector, Wonder Canine, Sir Barks-a-Lot
-// Animal: ${capitalizedAnimal}
-// Names:`;
-// };
-
 // console.log(openai); //  basePath: 'https://api.openai.com/v1',
 // console.log(configuration);
 // const start = async (req, res) => {
@@ -40,11 +27,11 @@ const generate = async (req, res) => {
     return;
   }
 
-  const animal = req.body.animal || '';
-  if (animal.trim().length === 0) {
+  const requestData = req.body.requestMobile || '';
+  if (requestData.trim().length === 0) {
     res.status(400).json({
       error: {
-        message: 'Please enter a valid animal',
+        message: 'Please enter a valid requestData',
       },
     });
     return;
@@ -60,12 +47,9 @@ const generate = async (req, res) => {
       // model: 'gpt-4', // stable
       // model: 'gpt-4-0314', // deprecated June 14th, 2023
       // model: 'gpt-4-32k-0314', // deprecated June 14th, 2023
-
-      // prompt: generatePrompt(animal),
-      // prompt: animal, // model: 'text-davinci-003',
       temperature: 0.6,
       max_tokens: 25,
-      messages: [{ role: 'user', content: animal }],
+      messages: [{ role: 'user', content: requestData }],
     });
     console.log(completion.data.choices[0].message.content);
     // res.status(200).json({ result: completion.data.choices[0].text.trim() }); // model: 'text-davinci-003',
