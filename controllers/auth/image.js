64ecbaf925baca5cfc1afa1de52e75c4c2856cb9
kovalenkoandri <kovalenkoandrii@gmail.com>
2023-04-12 14:11:03 +0300

    req.body.prompt

--- controllers/auth/image.js
+++ controllers/auth/image.js
@@ -16,7 +16,7 @@ const image = async (req, res) => {
     return;
   }
 
-  const requestData = req.body.requestMobile || '';
+  const requestData = req.body.prompt || '';
   if (requestData.trim().length === 0) {
     res.status(400).json({
       error: {
